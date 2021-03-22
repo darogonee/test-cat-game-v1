2821c00f65b188450f550675c2a83224c0e952de
darogonee <80794515+darogonee@users.noreply.github.com>
2021-03-22 21:13:17 +1100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
+scene.setBackgroundColor(9)
 let player1 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . f f . . 
     . . . . . . . . . . . . f f f . 
-    . . . . . . . . . . . . f 5 1 . 
-    . . . . . . . . . . . . f f f . 
+    . . . . . . . . . . . . f f 5 f 
+    f f f f f f f f f f f f f f f f 
+    . . . . f f f f f f f f f f . . 
     . . . . f f f f f f f f f . . . 
-    . . . . f f f f f f f f f . . . 
-    . . . . f f . . . . . f f . . . 
-    . . . . f f . . . . . f f . . . 
-    . . . . f f . . . . . f f . . . 
+    . . . . f . f . . . f . f . . . 
+    . . . . f . f . . . f . f . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(player1)
+controller.moveSprite(player1, 100, 100)
+tiles.setTilemap(tilemap`level1`)
